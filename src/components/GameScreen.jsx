a861d0197062a019dfa8d27db883e9ae763ac024
w-usagi <worklk730@gmail.com>
2026-05-16 20:00:01 +0900

    add: character

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,12 +1,25 @@
+import speakerImage from "../assets/ANNX_PlanB.JPG";
+
 function GameScreen({ currentWord, onAnswer, version }) {
   return (
     <div className="game-screen">
+      <div className="version">
+        v{version}
+      </div>
 
-        <div className="version">
-            v{version}
+      <div className="speaker-area">
+
+        <img
+          src={speakerImage}
+          alt="speaker"
+          className="speaker-image"
+        />
+
+        <div className="speech-bubble">
+          {currentWord}
         </div>
 
-      <div className="word">{currentWord}</div>
+      </div>
 
       <div className="buttons">
         <button onClick={() => onAnswer("らっきょう")}>
