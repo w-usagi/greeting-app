566d1f828fb0a1b38f250e7bf389a61dc2343657
tanab <worklk730@gmail.com>
2026-05-16 17:50:34 +0900

    adjust: ranking display

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -3,13 +3,33 @@ function Ranking({ ranking }) {
     <div className="ranking">
       <h2>ランキング TOP5</h2>
 
-      <ol>
+        <div className="ranking-list">
+        {ranking.map((score, index) => (
+          <div key={index} className="ranking-item">
+
+            <span className="rank-number">
+              {index + 1}位
+            </span>
+
+            <span className="rank-score">
+              {score}
+            </span>
+
+            <span className="rank-point">
+              point
+            </span>
+
+          </div>
+        ))}
+      </div>
+
+      {/* <ol>
         {ranking.map((score, index) => (
           <li key={index}>
             {score} point
           </li>
         ))}
-      </ol>
+      </ol> */}
     </div>
   );
 }
